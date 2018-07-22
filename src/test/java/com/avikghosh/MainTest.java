f859276fa1bd6d5185fbf07ce87cb2485de1f27c
avikghosh <avik.ghosh92@gmail.com>
2018-07-22 13:18:31 +0530

    Save Screenshots with Test Case Folder

--- src/test/java/com/avikghosh/MainTest.java
+++ src/test/java/com/avikghosh/MainTest.java
@@ -16,15 +16,16 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class MainTest {
+public class MainTest  {
     private AndroidDriver<AndroidElement> driver;
 
     @BeforeTest
     public void setupAppium() throws MalformedURLException {
-        System.out.println("In Before Test, setting up Appium Driver");
+        System.out.println("Setup Driver");
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("deviceName", "emulator-5554");
@@ -32,12 +33,14 @@ public class MainTest {
         desiredCapabilities.setCapability("appPackage", "com.android.dialer");
         desiredCapabilities.setCapability("appActivity", ".DialtactsActivity");
         driver = new AndroidDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), desiredCapabilities);
+        System.out.println("Driver Created Successfully");
 
     }
 
     @Test
-    public void appiumDemoTest() {
+    public void appiumDemoTest(Method method) {
         int screenshotCounter = 1;
+        String testMethodName = method.getName();
         System.out.println("Screenshots will be output to : " + System.getProperty("user.dir") + "/screenshots/");
         By addFavoriteLink = By.id("com.android.dialer:id/emptyListViewAction");
         By emptyContactMsg = By.id("com.android.dialer:id/emptyListViewMessage");
@@ -45,9 +48,9 @@ public class MainTest {
         WebDriverWait wait = new WebDriverWait(driver, 5);
         ExpectedCondition addFavoriteCondition = ExpectedConditions.presenceOfElementLocated(addFavoriteLink);
         wait.until(addFavoriteCondition);
-        saveScreenshot(screenshotCounter++);
+        saveScreenshot(testMethodName, screenshotCounter++);
         driver.findElement(addFavoriteLink).click();
-        saveScreenshot(screenshotCounter++);
+        saveScreenshot(testMethodName, screenshotCounter++);
         ExpectedCondition emptyContactsCondition = ExpectedConditions.presenceOfElementLocated(emptyContactMsg);
         wait.until(emptyContactsCondition);
         String actualText = driver.findElement(emptyContactMsg).getText();
@@ -55,22 +58,24 @@ public class MainTest {
             Assert.assertEquals(actualText, expectedText);
             System.out.println("Empty Contacts Text Assertion Passed, Actual: " + actualText + " Expected: "+ expectedText);
         } catch (AssertionError e) {
-            Assert.fail("Empty Contacts Text Assertion Failed, Actual: " + actualText + " Expected: "+ expectedText);
+            System.out.println("Empty Contacts Text Assertion Failed, Actual: " + actualText + " Expected: "+ expectedText);
+            Assert.fail();
         }
-        saveScreenshot(screenshotCounter++);
+        saveScreenshot(testMethodName, screenshotCounter++);
 
     }
 
     @AfterTest
     public void tearDown() {
-        System.out.println("In After Test, closing driver");
         driver.quit();
+        System.out.println("Driver teardown successful.");
     }
 
-    private void saveScreenshot(int screenshotCounter) {
+    private void saveScreenshot(String testMethodName, int screenshotCounter) {
         try {
             File screenshot = driver.getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(screenshot, new File(System.getProperty("user.dir") + "/screenshots/"+ screenshotCounter + ".png"));
+            FileUtils.copyFile(screenshot, new File(System.getProperty("user.dir") + "/screenshots/" + testMethodName + "/ " + screenshotCounter + ".png"));
+            System.out.println("Screenshot successfully saved to " + System.getProperty("user.dir") + "/screenshots/" + testMethodName + "/ " + screenshotCounter + ".png");
         } catch (IOException e) {
             System.out.println("There was an exception in copying the file to the required directory" + e.getMessage());
         }
